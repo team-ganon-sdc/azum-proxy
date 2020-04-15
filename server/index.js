d5980bbb378a31a8f492c81f7cece3f6c7846ba3
Azum Beg <azumbeg@gmail.com>
2020-04-15 12:32:40 -0500

    Add New Relic and Artillery

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const port = 3000;
+// const cluster = require("cluster");
 
-// app.use(express.static('public'))
-app.use(express.static(path.join(__dirname, '/../public')));
+// if (cluster.isMaster) {
+//   const numWorkers = require("os").cpus().length;
+//   console.log(`Master cluster setting up ${numWorkers} workers...`)
 
-app.listen(port, () => console.log(`Proxy server running on port ${port}!`));
+//   for (let i = 0; i < numWorkers; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on("online", worker => {
+//     console.log(`Worker ${worker.process.pid} is online`);
+//   });
+
+//   cluster.on("exit", (worker, code, signal) => {
+//     console.log(`Worker ${worker.process.pid} died with code: ${code} and signal: ${signal}`);
+//     console.log("Starting a new worker");
+//     cluster.fork();
+//   });
+// } else {
+  require("newrelic");
+  const express = require('express');
+  const path = require('path');
+  const app = express();
+  const port = 3000;
+
+  app.use(express.static(path.join(__dirname, '/../public')));
+
+  app.listen(port, () => console.log(`Proxy server running on port ${port}!`));
+// }
