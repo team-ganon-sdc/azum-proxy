b893605d67c8c4afb7c7b6ef06d0f013c9e42b60
Azum Beg <azumbeg@gmail.com>
2020-04-16 12:53:15 -0500

    Implement New Relic and Artillery

--- server/index.js
+++ server/index.js
@@ -1,30 +1,49 @@
-// const cluster = require("cluster");
+const cluster = require("cluster");
 
-// if (cluster.isMaster) {
-//   const numWorkers = require("os").cpus().length;
-//   console.log(`Master cluster setting up ${numWorkers} workers...`)
+if (cluster.isMaster) {
+  const numWorkers = require("os").cpus().length;
+  console.log(`Master cluster setting up ${numWorkers} workers...`)
 
-//   for (let i = 0; i < numWorkers; i++) {
-//     cluster.fork();
-//   }
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork();
+  }
 
-//   cluster.on("online", worker => {
-//     console.log(`Worker ${worker.process.pid} is online`);
-//   });
+  cluster.on("online", worker => {
+    console.log(`Worker ${worker.process.pid} is online`);
+  });
 
-//   cluster.on("exit", (worker, code, signal) => {
-//     console.log(`Worker ${worker.process.pid} died with code: ${code} and signal: ${signal}`);
-//     console.log("Starting a new worker");
-//     cluster.fork();
-//   });
-// } else {
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died with code: ${code} and signal: ${signal}`);
+    console.log("Starting a new worker");
+    cluster.fork();
+  });
+} else {
   require("newrelic");
   const express = require('express');
   const path = require('path');
+  const bodyParser = require('body-parser');
+  const axios = require('axios');
+
   const app = express();
   const port = 3000;
 
   app.use(express.static(path.join(__dirname, '/../public')));
 
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  app.post("/reviews", (req, res) => {
+    const { item, author, body, rating, likes } = req.body;
+    axios.post("http://localhost:3002/reviews", {
+        item,
+        author,
+        body,
+        rating,
+        likes,
+      })
+      .then(result => res.sendStatus(200))
+      .catch(err => res.json(err));
+  });
+
   app.listen(port, () => console.log(`Proxy server running on port ${port}!`));
-// }
+}
