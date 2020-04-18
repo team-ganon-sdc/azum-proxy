99687b8c68568f593bcd5274c27689e785fb2e83
Azum Beg <azumbeg@gmail.com>
2020-04-18 11:19:58 -0500

    Implement attempt at server side rendering

--- server/index.js
+++ server/index.js
@@ -21,11 +21,16 @@ if (cluster.isMaster) {
   require("newrelic");
   const express = require('express');
   const path = require('path');
+  const fs = require('fs');
   const bodyParser = require('body-parser');
   const axios = require('axios');
 
   const app = express();
   const port = 3000;
+
+  app.use("^/$", (req, res) => {
+
+  });
 
   app.use(express.static(path.join(__dirname, '/../public')));
 
